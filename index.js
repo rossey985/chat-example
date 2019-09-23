51321c5321e4afed3e4598b876596cbeac9f8cc6
ChristianGigante <christian.gigante@student.passerellesnumeriques.org>
2019-09-23 21:07:24 +0800

    Changes are still in

--- index.js
+++ index.js
@@ -1,18 +1,44 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+let app = require('express')();
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
-});
+let http = require('http').Server(app);
 
-io.on('connection', function(socket){
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
-  });
-});
+let io = require('socket.io')(http);
 
-http.listen(port, function(){
-  console.log('listening on *:' + port);
-});
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+io.on('connection', (socket) => {
+    io.emit('noOfConnections', Object.keys(io.sockets.connected).length)
+
+
+    socket.on('disconnect', () => {
+        console.log('disconnected')
+        io.emit('noOfConnections', Object.keys(io.sockets.connected).length)
+    })
+
+
+
+    socket.on('chat-message', (msg) => {
+        socket.broadcast.emit('chat-message', msg)
+    })
+    socket.on('joined', (name) => {
+        socket.broadcast.emit('joined', name)
+    })
+    socket.on('leaved', (name) => {
+        socket.broadcast.emit('leaved', name)
+    })
+
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data)
+    })
+    socket.on('stoptyping', () => {
+        socket.broadcast.emit('stoptyping')
+    })
+
+
+})
+
+http.listen(3000, () => {
+    console.log('Server is started at http://localhost:3000')
+})
